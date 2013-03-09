cb1b926a07aeea8bc4c9dadec6b7e6eb5ddfeee0
Eric Martindale <eric@ericmartindale.com>
2013-03-09 17:15:14 +0000

    Add GitHub keys.

--- config.js
+++ config.js
@@ -5,7 +5,11 @@
 
 module.exports = {
     appPort: 9200
-  , callbackURL: "http://yourapp.com:9200" // set this to the public address of your app
+  , callbackURL: "http://forkshop.ericmartindale.com" // set this to the public address of your app
   , cookieSecret: 'this can be any random string, you can even use this one. :)'
-  , databaseName: 'tswedu'
+  , databaseName: 'forkshop'
+  , github: {
+        clientID: '033d055c9fb974924671'
+      , clientSecret: '1b5a416c77d27101e42dc6db3a17a14b9948148c'
+    }
 };
