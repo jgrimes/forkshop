6f03c93d43b2168e8853d2eec8bf956e715983d0
Matt Hoffman <matt@mhoffman.org>
2013-03-09 22:13:34 -0500

    Change "create" to fork a template
    Also, move the git calls to be within the original save() callback (thanks Eric)

--- controllers/course.js
+++ controllers/course.js
@@ -14,7 +14,8 @@ module.exports = {
       var course = new Course({
           name: req.param('courseName')
         , description: req.param('description')
-        //, _creator: req.user._id
+        , _creator: req.user._id
+        , _owner: req.user._id
       });
 
       course.save(function(err) {
@@ -27,51 +28,47 @@ module.exports = {
       var course = new Course({
           name: req.param('name')
         , description: req.param('description')
-        //, _creator: req.user._id
+        , _creator: req.user._id
+        , _owner: req.user._id
       });
-
+      var util = require("./util");
+      util.github(req.user.github.token)
       course.save(function(err) {
-        res.redirect( '/courses/' + course._id);
-      });
+        // save to Git...
+        console.log("creating a repo named "+course.name);
 
-      // save to Git...
-      console.log("creating a repo named "+course.name);
-
-      var GitHubApi = require("github");
-      var github = new GitHubApi({
-        // required
-        version: "3.0.0"
-        // optional
-      , timeout: 5000
-      });
-      github.authenticate({
-        //type: "oauth" // obviously, make OAuth happen here.
-        //, token: req.user.github.token // we're assuming this is here for now. Nulls? We don't handle no stinkin' nulls.
-          type: "basic"
-        , username: 'coursefork-test'
-        , password: 'coursefork001'
-      });
-
-     // this block is for debugging
-     //github.user.getFollowingFromUser({
-     //  user: "coursefork-test"
-     //}, function(err, res) {
-     //  console.log(JSON.stringify(res));
-     //});
-
-      github.repos.create({
-        "name": course.name,
-        "description": course.description,
-        "homepage": "https://github.com",
-        "private": false,
-        "has_wiki": true
-      }, function(err, res) {
-        console.log("Got err?", err);
-        console.log("Got res?", res);
-
-      })
-
-
+        github.repos.fork({
+            "user": courseOwnerName
+          , "repo": courseName
+        }, function(forkerr, forkres) {
+          console.log("Oh fork:", forkerr);
+          if (forkres) {
+            // this is where we create a new course
+            console.log("forkin' A", forkres);
+              var templateUser = "coursefork";
+              var templateName = "course-template";
+              github.repos.fork({
+                  "user": templateUser
+                , "repo": templateName
+              }, function(err, res) {
+                console.log("Got err?", err);
+                console.log("Got res?", res);
+                github.repos.update({
+                    "user": course._owner.github.username
+                  , "repo": templateName
+                  , "name": course.name
+                  , "description": course.description
+                  , "homepage": "https://coursefork.org"
+                  , "private": false
+                  , "has_wiki": true
+                })
+                res.redirect( '/courses/' + course._id);
+              })
+          } else {
+            res.redirect("/error");//...or something. Whatever.
+          }
+        });
+     });
   }
   , view: function(req, res, next) {
       Course.findOne({ _id: req.param('courseID') }).populate("_owner").exec(function(err, course) {
